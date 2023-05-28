93611ac11cbbc9143bede7adfb38e103c3f3b3a4
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-05-28 14:19:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-//your JS code here. If required.
-const sounds = [];
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
 
-for (let i = 1; i <= 6; i++) {
-  sounds.push(new Promise((resolve, reject) => {
-    const sound = new Audio(`sounds/audio${i}.mp3`);
-    sound.addEventListener('canplay', () => {
-      resolve(sound);
-    });
-    sound.addEventListener('error', (error) => {
-      reject(error);
-    });
-  }));
+function stop() {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
 }
-
-Promise.all(sounds)
-  .then((loadedSounds) => {
-    const buttons = document.querySelectorAll('.btn');
-    const stopButton = document.querySelector('.stop');
-
-    function playSound(index) {
-      stopSounds();
-      loadedSounds[index].play();
-    }
-
-    function stopSounds() {
-      loadedSounds.forEach((sound) => {
-        sound.pause();
-        sound.currentTime = 0;
-      });
-    }
-
-    buttons.forEach((button, index) => {
-      button.addEventListener('click', () => {
-        playSound(index);
-      });
-    });
-
-    stopButton.addEventListener('click', () => {
-      stopSounds();
-    });
-  })
-  .catch((error) => {
-    console.error('Failed to load sounds', error);
-  });
