4191b64d3374351c42fe97a6e2506b3e4233c9eb
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-05-28 14:16:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 //your JS code here. If required.
-//your JS code here. If required.
-let appl = document.getElementById("appl");
-let boo = document.getElementById("boo");
-let gasp = document.getElementById("gasp");
-let tada = document.getElementById("tada");
-let vic = document.getElementById("vic");
-let wrg = document.getElementById("wrg");
-let aud = document.getElementById("audio");
-
-appl.addEventListener('click', myFunc);
-boo.addEventListener('click', myFunc);
-gasp.addEventListener('click', myFunc);
-tada.addEventListener('click', myFunc);
-vic.addEventListener('click', my//your JS code here. If required.
-const buttons = document.getElementById("buttons").children;
-const stopButton = document.getElementsByClassName("stop")[0];
 const sounds = [];
 
-// Push all sounds in the "sounds" array
-for (let i = 1; i <= buttons.length - 1; i++) {
-  sounds.push(new Audio(`sounds/sound${i}.mp3`));
+for (let i = 1; i <= 6; i++) {
+  sounds.push(new Promise((resolve, reject) => {
+    const sound = new Audio(`sounds/audio${i}.mp3`);
+    sound.addEventListener('canplay', () => {
+      resolve(sound);
+    });
+    sound.addEventListener('error', (error) => {
+      reject(error);
+    });
+  }));
 }
 
-// Add event listeners to the buttons
-for (let i = 0; i < buttons.length - 1; i++) {
-  buttons[i].addEventListener("click", () => {
-    sounds[i].currentTime = 0;
-    sounds[i].play();
+Promise.all(sounds)
+  .then((loadedSounds) => {
+    const buttons = document.querySelectorAll('.btn');
+    const stopButton = document.querySelector('.stop');
+
+    function playSound(index) {
+      stopSounds();
+      loadedSounds[index].play();
+    }
+
+    function stopSounds() {
+      loadedSounds.forEach((sound) => {
+        sound.pause();
+        sound.currentTime = 0;
+      });
+    }
+
+    buttons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        playSound(index);
+      });
+    });
+
+    stopButton.addEventListener('click', () => {
+      stopSounds();
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to load sounds', error);
   });
-}
-
-// Add event listener to the stop button
-stopButton.addEventListener("click", () => {
-  sounds.forEach(sound => sound.pause());
-});
-Func);
-wrg.addEventListener('click', myFunc);
-
-function myFunc() {
-	aud.play();
-}
-function pause() {
-	aud.pause();	
-}
