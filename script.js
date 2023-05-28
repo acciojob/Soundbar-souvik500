0660b09f39a7130a0e88824685d71342ed1619f6
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-05-28 14:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,29 @@ appl.addEventListener('click', myFunc);
 boo.addEventListener('click', myFunc);
 gasp.addEventListener('click', myFunc);
 tada.addEventListener('click', myFunc);
-vic.addEventListener('click', myFunc);
+vic.addEventListener('click', my//your JS code here. If required.
+const buttons = document.getElementById("buttons").children;
+const stopButton = document.getElementsByClassName("stop")[0];
+const sounds = [];
+
+// Push all sounds in the "sounds" array
+for (let i = 1; i <= buttons.length - 1; i++) {
+  sounds.push(new Audio(`sounds/sound${i}.mp3`));
+}
+
+// Add event listeners to the buttons
+for (let i = 0; i < buttons.length - 1; i++) {
+  buttons[i].addEventListener("click", () => {
+    sounds[i].currentTime = 0;
+    sounds[i].play();
+  });
+}
+
+// Add event listener to the stop button
+stopButton.addEventListener("click", () => {
+  sounds.forEach(sound => sound.pause());
+});
+Func);
 wrg.addEventListener('click', myFunc);
 
 function myFunc() {
